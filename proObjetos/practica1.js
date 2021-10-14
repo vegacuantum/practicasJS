421958ec0e7e0fce8f4900f91dc754b0b578065f
Cuantico <vegacuantum@hotmail.com>
2021-10-14 09:45:19 -0500

    Aprendiendo js

---
+++ proObjetos/practica1.js
@@ -0,0 +1,10 @@
+//Mi primer objeto literal natalia
+const natalia = {
+    name: "natalia",
+    age: 20,
+    cursosAprobados: [
+        "curso 1",
+        "curso 2",
+    ]
+};
+//este objeto necesita un método
